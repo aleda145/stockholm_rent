0d6e4af4bcc8da6696130aebb15c513f2cc14b15
Alexander Dahl <alex@dahl.dev>
2019-12-19 23:00:46 +0100

    init commit

---
+++ analysis.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+import json
+from pandas.io.json import json_normalize
+with open('qasa_stockholm.json', 'r') as f:
+
+    load_data = json.load(f)
+
+print(load_data)
+df = json_normalize(load_data['homes'])
+
+print(df)
+#df = pd.read_json('qasa_stockholm.json', orient='columns')
+#print(df)
